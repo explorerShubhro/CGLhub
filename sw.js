37ab82a7780c6cbbd958ae55deb15a1d1246dfd2
explorerShubhro <124862888+explorerShubhro@users.noreply.github.com>
2026-06-04 16:58:34 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'focusdesk-v2';
+const ASSETS = [
+  '/CGLhub/',
+  '/CGLhub/index.html',
+  '/CGLhub/manifest.json',
+  '/CGLhub/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
